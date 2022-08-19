d66ee76815beaba3325ae6da0d0ba63215bf94d9
Utkarsh290801 <utarora3@gmail.com>
2022-08-19 13:58:31 +0530

    ..

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,8 @@ const cors = require("cors");
 app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.json());
 app.get("/home", (req, res) => {
-    res.send("hello express");
-  });
-  app.listen(port, () => {
-    console.log("the server has been started");
-  });
-  
+  res.send("hello express");
+});
+app.listen(port, () => {
+  console.log("the server has been started");
+});
