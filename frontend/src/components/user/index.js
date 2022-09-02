f3b3739ba1ecd67b416dbc685558bb726827e8ef
Utkarsh Arora <utarora3@gmail.com>
2022-09-03 00:41:08 +0530

    ..

--- frontend/src/components/user/index.js
+++ frontend/src/components/user/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from './Header'
+import Header1 from './Header1'
 
 const User = () => {
   return (
     <div>
        <Header></Header>
+       <Header1/>
         <Outlet/>
     </div>
   )
