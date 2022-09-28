748e33bf67f95bd971f449cbb3f92c06ee036ab5
Utkarsh Arora <utarora3@gmail.com>
2022-09-28 22:56:41 +0530

    ...

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,23 +1,22 @@
 const { Schema, model } = require("../connection");
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 const myschema = new Schema({
   username: String,
   email: String,
   password: String,
-  newPassword: String,
-  confirmPassword: String,
+  // newPassword: String,
+  // confirmPassword: String,
   avatar: String,
   isAdmin: { type: Boolean, default: false },
   createdAt: { type: Date, default: new Date() },
 });
-// myschema.pre('save', function (next){
-//   console.log("hi from inside");
-//   if(this.isModified('password')){
-//     this.password=bcrypt.hash(this.password,12);
-//     // this.confirmpassword= bcrypt.hash(this.confirmpassword,12);
-//     // this.newpassword= bcrypt.hash(this.newpassword,12);
+myschema.pre('save', function (next){
+  console.log("hi from inside");
+  if(this.isModified('password')){
+    this.password=bcrypt.hash(this.password,12);
+    
 
-//   }
-//   next();
-// })
+  }
+  next();
+})
 module.exports = model("usercollection", myschema);
