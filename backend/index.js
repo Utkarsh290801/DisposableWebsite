7b0f04897d9e9da22a9ac44fcfd8a73c6f2c42cd
mansha126 <manshatiwari2612@gmail.com>
2022-10-27 14:29:34 +0530

    ...

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+const stripe = require('stripe')('sk_test_51LxR1fSIiQlrWSHJzTPUErNmkBIiQjlTwbD4HBEx64rAUF3fZvWhmV0OF1WnaG3FpRJOvwo9EVBxM8cOxDbl8lpJ008uJGWEBo'); //1
 const express = require('express')
 const app = express()
 const userRouter = require('./routers/userRouter')
@@ -8,6 +9,9 @@ const contactRouter = require("./routers/contactRouter");
 const utilRouter = require("./routers/utils");
 const cors = require('cors')
 const api_config = require('./config')
+
+app.use(express.static('public')); //2
+
 app.use(cors({ origin: 'http://localhost:3000' }))
 
 app.use(express.json())
@@ -22,7 +26,27 @@ app.use("/util", utilRouter);
 app.get('/', (req, res) => {
     res.send('response from userRouter')
 })
+
+app.post('/create-checkout-session', async (req, res) => {
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+          price: '{{PRICE_ID}}',
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: `${'http://localhost:3000'}?success=true`,
+      cancel_url: `${'http://localhost:3000'}?canceled=true`,
+    });
+  
+    res.redirect(303, session.url);
+  }); //3
+
 app.use(express.static('./static/uploads'))
 app.listen(port, () => {
     console.log('server started')
 })
+
+app.listen(4242, () => console.log('Running on port 4242')); //4
